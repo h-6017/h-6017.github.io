933f8ee8e2bba4b70b601ff4ac50fcfc1dbc43d0
Brandon Brown <bbrown_h@dev.bse.edu>
2015-05-07 17:30:43 -0500

    Workin at the coal mine

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -4,8 +4,8 @@ function getBlog() {
     $.get('entries.json', function(data) {
         alert("we got the file");
         $.each(data.entries, function(key, val) {
-            alert("reading the file");
             console.log(key + " " + val.date);
+
         });
     });
 }
