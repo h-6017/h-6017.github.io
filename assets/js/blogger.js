dbec9607f4ebf9f10d1e17cb37e2732ae409edf8
unknown <BBrown_H@Dev01.bsecode.inc>
2015-05-07 11:42:08 -0500

    Changing structure of blog

---
+++ assets/js/blogger.js
@@ -0,0 +1,14 @@
+//this is my new blog... isn't it great?
+$(document).ready(getblog);
+/*
+function getblog(){
+    var entries = $.getJSON('entries.json');
+    $('#blog').html($.parseJSON(entries));
+*/    
+function getblog(){
+    $.getJSON('entries.json', function(data) {
+$.each(data.entries, function(key, val) {
+                       console.log(key + " " + val.date);
+                 }
+     }
+}
