e4a1d028cf33aa841e94a60bbafe20b424c0e059
unknown <BBrown_H@Dev01.bsecode.inc>
2015-05-07 13:50:32 -0500

    ts

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,7 +1,7 @@
 //this is my new blog... isn't it great?
 function getBlog() {
     alert("infunction");
-    $.getJSON('entries.json', function(data) {
+    $.getJSON('entries.JSON', function(data) {
         alert("we got the file");
         $.each(data.entries, function(key, val) {
             alert("reading the file");
