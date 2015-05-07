327c044f3db6e91d4866175b7b76da32276b7019
unknown <BBrown_H@Dev01.bsecode.inc>
2015-05-07 14:35:20 -0500

    commitment is scary

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,14 +1,14 @@
 //this is my new blog... isn't it great?
 function getBlog() {
     alert("infunction");
-    $.get('entries.json', function(data) {
+    $.getJSON('entries.json', function(data) {
         alert("we got the file");
         $.each(data.entries, function(key, val) {
             alert("reading the file");
             console.log(key + " " + val.date);
         });
     });
-};
+}
 $(document).ready(function(){
     alert("My body is ready");
     getBlog();
