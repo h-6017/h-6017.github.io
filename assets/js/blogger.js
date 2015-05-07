d24c7605b3c29dc512f99a5ab55dc1a82fe19609
unknown <BBrown_H@Dev01.bsecode.inc>
2015-05-07 11:57:25 -0500

    Fooo

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,16 +1,17 @@
 //this is my new blog... isn't it great?
-$(document).ready(getblog);
+$(document).ready(function(){
 /*
 function getblog(){
     var entries = $.getJSON('entries.json');
     $('#blog').html($.parseJSON(entries));
 */    
 function getblog(){
-alert("Im working");
     $.getJSON('entries.json', function(data) {
-$.each(data.entries, function(key, val) {
-                       console.log(key + " " + val.date);
-});
-     });
+        $.each(data.entries, function(key, val) {
+            console.log(key + " " + val.date);
+        });
+    });
 }
-///
+getblog();
+
+});
