f21880419f1ddb1553cefd8bcd393c9dabc64e22
Brandon Brown <bbrown_h@dev.bse.edu>
2015-05-08 09:47:53 -0500

    Functional jquery blog generation achieved

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,8 +1,6 @@
 //this is my new blog... isn't it great?
 function getBlog() {
-    alert("infunction");
     $.get('entries.json', function(data) {
-        alert("we got the file");
         $.each(data.entries, function(key, val) {
 
             console.log("Selecting Blog number " + key + " for next generation.");
@@ -20,7 +18,6 @@ function getBlog() {
     });
 }
 $(document).ready(function(){
-    alert("My body is ready");
     getBlog();
 });
 
