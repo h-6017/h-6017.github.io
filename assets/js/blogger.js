1160fb2abcb8a24cb2e2401391325677345a105f
Brandon Brown <bbrown_h@dev.bse.edu>
2015-05-18 08:34:31 -0500

    Blog commit

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,24 +1,36 @@
 //this is my new blog... isn't it great?
-/*
-$('#search2').on("keyup", function(){
-    alert("Im working");
+
+function search2() {
     $('.blogpost').hide();
+    console.log("Im working");
     var searchText = $(this).val().toLowerCase();
+    var results = []
     $('.blogpost').each(function(){
-        var targetText = $(this).val().toLowerCase();
+        var paraKids = $(this).children('p')
+        var targetText = paraKids[0].textContent.toLowerCase();
         if(targetText.indexOf(searchText) != -1) {
-            $(this).show();
-        }else{
-            $('.blogpost').show();
+            results.push(this);
         }
     });
-});
-*/
-function pager() {
-    var items = $('.blogpost');
+
+    $(document).trigger("search-started", {results: results}, pager);
+}
+function pager(evt, data) {
+    console.log(evt, data)
+    var items;
+    if (data) {
+        items = data.results;
+        $('.blogpost').hide()
+        $(items).show();
+    } else {
+        items = $('.blogpost');
+    }
     var numItems = items.length;
     var perPage = 2;
-    items.slice(perPage).hide();
+    var stuffToHide = items.slice(perPage)
+    $(stuffToHide).each(function() {
+        $(this).hide()
+    })
 
     $('#blogcontent').pagination( {
         items:numItems,
@@ -30,28 +42,31 @@ function pager() {
             $('.ellipse').hide();
             var start = perPage * (pageNum - 1);
             var end = start + perPage;
-            items.hide()
+            $(items).hide()
                  .slice(start, end).show();
             if(pageNum == 1) {
                 $('.current.prev').hide();
                 $('.page-link.prev').hide();
-            }else{
+            } else {
                 $('.current.prev').show();
                 $('.page-link.prev').show();
             }
             if(pageNum == Math.floor((numItems/perPage)+1)) {
                 $('.page-link.next').hide();
                 $('.current.next').hide();
-            }else {
+            } else {
                 $('.page-link.next').show();
                 $('.current.next').show();
             }
         }      
    });
-        $('.page-link').hide();
-        $('.ellipse').hide();
-        $('.current').hide();
-        $('.page-link.next').show();
+    $('.page-link').hide();
+    $('.ellipse').hide();
+    $('.current').hide();
+    $('.page-link.next').show();
+    $('#search2').val('');
+    $('#search2').show();
+
 }
 
 
@@ -73,7 +88,7 @@ function getBlog() {
                 .append(content)
                 .append("<hr/>")
 
-            $('#blog').append(newBlogPost);
+            $('#blog').prepend(newBlogPost);
 
         });
         $(document).trigger('entries-loaded')
@@ -83,5 +98,12 @@ function getBlog() {
 $(document).ready(function(){
     getBlog();
     $(document).on('entries-loaded', pager);
+    $(document).on('search-started', pager);
+    $('#search2').keyup(function () {
+        $('#search-second').val($('#search2').val());
+        $(this).ready(search2);
+        $('#search-second').focus();
+        $('#search2').hide();
+        $('#search-second').keyup(search2);
+    });
 });
-
